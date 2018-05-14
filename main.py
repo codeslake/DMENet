1980f74516a41ef99d3222d8d0c8688a02b90120
codeslake <junyonglee@postech.ac.kr>
2018-05-14 20:17:54 +0900

    bug fixed

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def train():
         with tf.variable_scope('binary'):
             loss_real_binary = tl.cost.sigmoid_cross_entropy(output_real_binary_logits, labels_real_binary, name = 'real')
             #loss_real_binary = tl.cost.mean_squared_error(output_real_binary, labels_real_binary, is_mean = True, name = 'real')
-            loss_binary = tf.identity(loss_real_binary * 1e-2)
+            loss_binary = tf.identity(loss_real_binary)
 
         loss_main = tf.identity(loss_defocus + loss_binary + loss_perceptual + loss_aux + loss_g, name = 'total')
         loss_init = tf.identity(loss_defocus, name = 'loss_init')
