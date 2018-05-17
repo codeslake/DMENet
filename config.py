725cb0eba64de22f3a359542280cb531e0eca920
codeslake <junyonglee@postech.ac.kr>
2018-05-17 09:03:45 +0900

    bug fixed

--- config.py
+++ config.py
@@ -30,11 +30,12 @@ config.TRAIN.lambda_binary = 1e-2
 
 ## perceptual loss coefficient
 #config.TRAIN.lambda_perceptual = 2.5e-6
-config.TRAIN.lambda_perceptual = 1e-7
+#config.TRAIN.lambda_perceptual = 1e-7
+config.TRAIN.lambda_perceptual = 1e-6
 
 ## perceptual classification loss coefficient
 #config.TRAIN.lambda_perceptual_c = 5/16.
-config.TRAIN.lambda_perceptual_c = 1e-3
+config.TRAIN.lambda_perceptual_c = 1e-4
 
 ## train set location
 # config.TRAIN.synthetic_img_path = '/data1/junyonglee/blur_sharp_only/image_dof/'
