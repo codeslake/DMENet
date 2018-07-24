d3687f72ac14dad3125ef4ae83e86ff08fc15162
codeslake <junyonglee@postech.ac.kr>
2018-07-24 22:57:48 +0900

    bug fixed

--- config.py
+++ config.py
@@ -54,8 +54,10 @@ offset = '/Mango/Users/JunyongLee/hub/datasets/'
 config.TRAIN.real_img_path = offset + 'BlurDetection/train/image/'
 config.TRAIN.real_binary_map_path = offset + 'BlurDetection/train/gt/'
 
-config.TEST.real_img_path = offset + 'BlurDetection/test/image/'
-config.TEST.real_binary_map_path = offset + 'BlurDetection/test/gt/'
+#config.TEST.real_img_path = offset + 'BlurDetection/test/image/'
+#config.TEST.real_binary_map_path = offset + 'BlurDetection/test/gt/'
+config.TEST.real_img_path = offset + 'test/out/image/'
+config.TEST.real_binary_map_path = offset + 'test/out/blur_map_norm/'
 
 ## train image size
 config.TRAIN.height = 240
