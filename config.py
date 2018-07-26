52da0c8a85a041b7e57e50ddc52696389e513445
codeslake <junyonglee@postech.ac.kr>
2018-07-26 21:01:13 +0900

    bug fixed

--- config.py
+++ config.py
@@ -56,10 +56,10 @@ config.TRAIN.real_binary_map_path = offset + 'BlurDetection/train/gt/'
 
 # config.TEST.real_img_path = offset + 'BlurDetection/test/image/'
 # config.TEST.real_binary_map_path = offset + 'BlurDetection/test/gt/'
-# config.TEST.real_img_path = offset + 'test2/'
-# config.TEST.real_binary_map_path = offset + 'test2/'
-config.TEST.real_img_path = offset + 'syndof_test/'
-config.TEST.real_binary_map_path = offset + 'syndof_test/'
+config.TEST.real_img_path = offset + 'test2/'
+config.TEST.real_binary_map_path = offset + 'test2/'
+# config.TEST.real_img_path = offset + 'syndof_test/'
+# config.TEST.real_binary_map_path = offset + 'syndof_test/'
 
 ## train image size
 config.TRAIN.height = 240
